a58092d6c50f6639e9d483c3976d57dc5bcf043a
eduardorodriguez <eduasinco@gmail.com>
2020-05-28 13:32:30 -0700

    Some conversation improvements

--- ComeAquiOS/Utils/Strings.swift
+++ ComeAquiOS/Utils/Strings.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 let local_server = "http://0.0.0.0:65100"
-let real_local_server = "http://10.153.30.138:65100"
+let real_local_server = "http://10.153.31.109:65100"
 let production_server = "http://54.193.13.44"
 
 let SERVER = real_local_server
