de813a749bf2ef43ec9ff344826963213a2fcb9e
eduardorodriguez <eduasinco@gmail.com>
2020-06-06 11:34:38 -0700

    camera access check message

--- ComeAquiOS/Utilities/Pop Ups/GalleryCamera/GaleryCameraPopUpViewController.swift
+++ ComeAquiOS/Utilities/Pop Ups/GalleryCamera/GaleryCameraPopUpViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import AVFoundation
+
+
 protocol GaleryCameraPopUpProtocol {
     func image(_ image: UIImage)
 }
@@ -20,7 +23,10 @@ class GaleryCameraPopUpViewController: UIViewController, UIImagePickerController
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        popUp.roundCorners(radius: 8)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        checkCamera()
     }
     
     @IBAction func galleryPress(_ sender: Any) {
@@ -31,11 +37,23 @@ class GaleryCameraPopUpViewController: UIViewController, UIImagePickerController
     }
     
     func importImage(_ gallery: Bool){
-        let image = UIImagePickerController()
-        image.delegate = self
-        image.sourceType = gallery ? .photoLibrary : .camera
-        image.allowsEditing = false
-        self.present(image, animated: true){}
+        if gallery {
+            let image = UIImagePickerController()
+            image.delegate = self
+            image.sourceType = .photoLibrary
+            image.allowsEditing = false
+            self.present(image, animated: true){}
+        }
+        
+        if !gallery, UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let image = UIImagePickerController()
+            image.delegate = self
+            image.sourceType = .camera
+            image.allowsEditing = false
+            self.present(image, animated: true){}
+        } else {
+            
+        }
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -46,5 +64,48 @@ class GaleryCameraPopUpViewController: UIViewController, UIImagePickerController
             // error
         }
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func checkCamera() {
+        let authStatus = AVCaptureDevice.authorizationStatus(for: .video)
+        switch authStatus {
+        case .authorized: break // Do your stuff here i.e. allowScanning()
+        case .denied: alertToEncourageCameraAccessInitially()
+        case .notDetermined: alertPromptToAllowCameraAccessViaSetting()
+        default: alertToEncourageCameraAccessInitially()
+        }
+    }
+
+    func alertToEncourageCameraAccessInitially() {
+        let alert = UIAlertController(
+            title: "IMPORTANT",
+            message: "Camera access is needed in order for you to upload a photo",
+            preferredStyle: UIAlertController.Style.alert
+        )
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Allow Camera", style: .cancel, handler: { (alert) -> Void in
+            UIApplication.shared.open(URL.init(string: UIApplication.openSettingsURLString)!)
+        }))
+        present(alert, animated: true, completion: nil)
+    }
+
+    func alertPromptToAllowCameraAccessViaSetting() {
+        let alert = UIAlertController(
+            title: "IMPORTANT",
+            message: "Camera access is needed in order for you to upload a photo",
+            preferredStyle: UIAlertController.Style.alert
+        )
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel) { alert in
+            if AVCaptureDevice.devices(for: .video).count > 0 {
+                AVCaptureDevice.requestAccess(for: .video) { granted in
+                    DispatchQueue.main.async() {
+                        self.checkCamera()
+                        
+                    }
+                }
+            }}
+        )
+        present(alert, animated: true, completion: nil)
     }
 }
